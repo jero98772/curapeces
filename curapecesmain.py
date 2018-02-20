3f13116d7afcce8004120129ba0051850999b603
jero98772 <jero98772@gmail.com>
2018-02-20 17:39:18 -0500

    segundo ensayo

--- curapecesmain.py
+++ curapecesmain.py
@@ -1,11 +1,11 @@
-#pastel o ayudas 
+#pastel o ayudas
 #cv2.line ( image, starting point , end point , color , line thickness, line type)
 #cv2.circle ( image, center, radius, color of border, line thickness / fill type, line type)
 #cv2.ellipse ( image, center, axes lengths, rotation degree of ellipse, starting angle , ending angle, color, line thickness / fill type, line type)
 #cv2.rectangle ( image, upper left corner vertex, lower right corner vertex, line thickness / fill type, line type)
 #cv2.putText ( image, text, starting point of text, font type, font scale, color, linetype )
 
-
+from flask import Flask 
 import cv2
 import sys
 import argparse
