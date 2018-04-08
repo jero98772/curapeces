da5d83af1c2202a77fe1387312db73b0cf613c14
jero98772 <jero98772@gmail.com>
2018-04-08 10:34:38 -0500

    Apegando lo al codigo aviejo sin el de sijar raval

--- curapecesmain.py
+++ curapecesmain.py
@@ -6,7 +6,7 @@
 #cv2.putText ( image, text, starting point of text, font type, font scale, color, linetype )
 #PERDONA la orrografia grcias por la compremncion
 #importamos las librerias de opencv, numpy ,flask ,etc
-#lineas sin entender bien 
+#lineas sin entender bien
 #173
 import cv2
 import sys
@@ -59,7 +59,6 @@ if seccion == "camara":
 			             break
 
 #PERDONA la orrografia grcias por la comprencion
-                                 
         captura.relase()
         cv2.destroyAllWindos()
     if seleccion =="ich":
@@ -165,128 +164,61 @@ if seccion == "camara":
                 cv2.destroyAllWindows()
 if seccion == "imagen":
         print ("usted escogio imagen")
-            #este esun codigo de siraj raval con el cual estoy aprendiendo  gracias por su comprecion
 
-        
-            plt.figure(figsize=(10, 10))#la funcion figure pertenese a la clase plt con el parametro de figsise y 10 ,10 es un valor
+k = 7
+blancobajo= np.array([0,140,120] ,)#[0,140,120]
+blancoalto = np.array([255,220,255], )#[255,220,255]
+blancoperfect  = np.array(255)#este esta en escala de grises
+#variables
+pez_pruba = input() #el lo que pongas en el input va a ser el nombre del archivo
+imagen_pezgris =cv2.imread(pez_pruba, cv2.IMREAD_GRAYSCALE)
+imagen_pez =cv2.imread(pez_pruba)
+sinreflejos =cv2.GaussianBlur(imagen_pez,(k,k),0)
+sinreflejosdos =cv2.cvtColor(sinreflejos, cv2.COLOR_BGR2HSV)
+mascara = cv2.inRange(sinreflejos,blancobajo,blancoalto)
+v1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE ,(15,15))
+mascara_tapada = cv2.morphologyEx(mascara,cv2.MORPH_CLOSE,v1)
+mascara_retapada = cv2.morphologyEx(mascara_tapada,cv2.MORPH_OPEN,v1)
+#las varibles de imagen como son y como van a salir
+numerodeblancos1 = np.sum(mascara_tapada == blancoperfect)
+numerodeblancosperfectos1 = numerodeblancos1 /100
+if numerodeblancosperfectos1 > 10 and numerodeblancosperfectos1 <1000:
+    print (' imagen 1 tiene',numerodeblancosperfectos1,' puntos de ick')
+else:
+    print ('imagen 1 no tine ict')
+numerodeblancos2 = np.sum(mascara_retapada == blancoperfect)
+numerodeblancosperfectos2 = numerodeblancos2 /100
+if numerodeblancosperfectos2 > 10 and numerodeblancosperfectos2 <1000:
+    print ('imagen 2 tiene',numerodeblancosperfectos2,' puntos de ick')
+else:
+    print ('imagen 2 no tine ict')
+numerodeblancos3 = np.sum(mascara_tapada == blancoperfect)
+numerodeblancosperfectos3 = numerodeblancos3 /100
+if numerodeblancosperfectos3 > 10 and numerodeblancosperfectos3 <10000:
+    print ('imagen 3 tiene',numerodeblancosperfectos3,' puntos de ick')
+else:
+    print ('imagen 3 no tine ict')
+numerodeblancos4 = np.sum(imagen_pezgris == blancoperfect)
+numerodeblancosperfectos4 = numerodeblancos4 /100
+if numerodeblancosperfectos4 > 10 and numerodeblancosperfectos4 <1000:
+    print ('imagen 4 tiene',numerodeblancosperfectos4,' puntos de ick')
+else:
+    print ('imagen 4 no tine ict')
+#condicionales  para un promedio y solo para algunas varibles de imagens
+numerodeblancos = numerodeblancos1 +numerodeblancos2+numerodeblancos3+numerodeblancos4
+numerodeblancosperfecto = numerodeblancos /400
+if numerodeblancosperfecto > 10 and numerodeblancosperfecto <1000:
+    print ('imagenes en promedio tiene',numerodeblancosperfecto,' puntos de ick')
+else:
+    print (' las  4  imagenes no tine ict pero tine', numerodeblancosperfecto, 'puntos de refleojos')
+cv2.imshow ('ventana1',imagen_pez)#y decimos que nos muestre imagen_pez y la ventana sellama ventana y +
+cv2.imshow ('ventana2',imagen_pezgris)
+cv2.imshow ('ventana3',sinreflejos)
+cv2.imshow ('ventana4',sinreflejosdos)
+cv2.imshow ('ventana5',mascara)
+cv2.imshow ('ventana6',mascara_tapada)
+cv2.imshow ('ventana7',mascara_retapada)
 
-            plt.imshow(image, interpolation='nearest')# la funcion imshow pertenese ala clase plt con los parametros de image y este que aprendindo ("interpolation='nearest'")
 
-        def overlay_mask(mask, image):
-        	#make the mask rgb
-            rgb_mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
-            #calculates the weightes sum of two arrays. in our case image arrays
-            #input, how much to weight each.
-            #optional depth value set to 0 no need
-            img = cv2.addWeighted(rgb_mask, 0.5, image, 0.5, 0)
-            return img
-
-        def find_biggest_contour(image):
-            # Copy
-            image = image.copy()
-            #input, gives all the contours, contour approximation compresses horizontal,
-            #vertical, and diagonal segments and leaves only their end points. For example,
-            #an up-right rectangular contour is encoded with 4 points.
-            #Optional output vector, containing information about the image topology.
-            #It has as many elements as the number of contours.
-            #we dont need it
-            contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-            # Isolate largest contour
-            contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
-            biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
-
-            mask = np.zeros(image.shape, np.uint8)
-            cv2.drawContours(mask, [biggest_contour], -1, 255, -1)
-            return biggest_contour, mask
-
-        def circle_contour(image, contour):
-            # Bounding ellipse
-            image_with_ellipse = image.copy()
-            #easy function
-            ellipse = cv2.fitEllipse(contour)
-            #add it
-            cv2.ellipse(image_with_ellipse, ellipse, 2, cv2.CV_AA)
-            return image_with_ellipse
-
-        def find_strawberry(image):
-            #RGB stands for Red Green Blue. Most often, an RGB color is stored
-            #in a structure or unsigned integer with Blue occupying the least
-            #second least, and Red the third least. BGR is the same, except the
-            #order of areas is reversed. Red occupies the least significant area,
-            # Green the second (still), and Blue the third.
-            # we'll be manipulating pixels directly
-            #most compatible for the transofrmations we're about to do
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-            # Make a consistent size
-            #get largest dimension
-            max_dimension = max(image.shape)
-            #The maximum window size is 700 by 660 pixels. make it fit in that
-            scale = 700/max_dimension
-            #resize it. same width and hieght none since output is 'image'.
-            image = cv2.resize(image, None, fx=scale, fy=scale)
-
-            #we want to eliminate noise from our image. clean. smooth colors without
-            #dots
-            # Blurs an image using a Gaussian filter. input, kernel size, how much to filter, empty)
-            image_blur = cv2.GaussianBlur(image, (9, 9), 0)
-            #t unlike RGB, HSV separates luma, or the image intensity, from
-            # chroma or the color information.
-            #just want to focus on color, segmentation
-            image_blur_hsv = cv2.cvtColor(image_blur, cv2.COLOR_RGB2HSV)
-
-            # Filter by colour
-            # 0-10 hue
-            #minimum red amount, max red amount
-            min_red = np.array([255, 165, 0])
-            max_red = np.array([255, 140, 0])
-            #layer
-            mask1 = cv2.inRange(image_blur_hsv, min_red, max_red)
-
-            #birghtness of a color is hue
-            # 170-180 hue
-            min_red2 = np.array([255, 165, 0])
-            max_red2 = np.array([255, 140, 0])
-            mask2 = cv2.inRange(image_blur_hsv, min_red2, max_red2)
-
-            #looking for what is in both ranges
-            # Combine masks
-            mask = mask1 + mask2
-
-            # Clean up
-            #we want to circle our strawberry so we'll circle it with an ellipse
-            #with a shape of 15x15
-            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
-            #morph the image. closing operation Dilation followed by Erosion.
-            #It is useful in closing small holes inside the foreground objects,
-            #or small black points on the object.
-            mask_closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-            #erosion followed by dilation. It is useful in removing noise
-            mask_clean = cv2.morphologyEx(mask_closed, cv2.MORPH_OPEN, kernel)
-
-            # Find biggest strawberry
-            #get back list of segmented strawberries and an outline for the biggest one
-            big_strawberry_contour, mask_strawberries =(mask)
-
-            # Overlay cleaned mask on image
-            # overlay mask on image, strawberry now segmented
-            overlay = overlay_mask(mask_clean, image)
-
-            # Circle biggest strawberry
-            #circle the biggest one
-            circled = circle_contour(overlay, big_strawberry_contour)
-            show(circled)
-
-            #we're done, convert back to original color scheme
-            bgr = cv2.cvtColor(circled, cv2.COLOR_RGB2BGR)
-
-            return bgr
-
-        #read the image
-        image = cv2.imread('fish.jpg')
-        #detect it
-        result = find_strawberry(image)
-        #write the new image
-        cv2.imwrite('fish2.jpg', result)
-#PERDONA la orrografia grcias por la compremncion
+cv2.waitKey(0)
+cv2.destroyAllWindows()
