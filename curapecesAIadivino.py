a0d8ac65cd98b8b915a57328f7f4dfecd3950070
daniel <jero98772@gmail.com>
2018-10-29 20:19:16 -0500

    agregando multiples  elecciones para diferntes modelos de  AI

--- curapecesAIadivino.py
+++ curapecesAIadivino.py
@@ -14,17 +14,36 @@ from keras.models import load_model
 pez='./especimenesdeprueba/'+input()
 #### hacemos que  las imagenes se puedan escojer desde la carpeta especimenesdeprueba
 
-    
+
 imagenpez = cv2.imread(pez, cv2.IMREAD_COLOR)
 
 cv2.imshow ('ventana1',imagenpez)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 longitud, altura = 150, 150
-modelo = './modelo/modelo.h5'
-pesos_modelo = './modelo/pesos.h5'
-cnn = load_model(modelo)
-cnn.load_weights(pesos_modelo)
+       # modeloenfemedad = './modeloenfermo/modelopezenfermo.h5'
+       # pesos_modeloenfemedad = './modeloenfermo/pesospezenfermo.h5'
+       # cnn = load_model(modeloenfemedad)
+       # cnn.load_weights(pesos_modeloenfemedad)
+
+modelo_si_es_un_pez = './modelo6/modelo.h5' 
+
+modelopez_sano_o_enefermo ='./modelopez/modelopez.h5'
+
+modelo_de_la_enfemedad ='./modeloenfermo/modelopezenfermo.h5'
+
+pesos_modelo_si_es_un_pez = './modelo6/pesos.h5'
+
+pesospez_sano_o_enefermo ='./modelopez/pesospez.h5'
+
+pesos_modelo_de_la_enfemedad='./modeloenfermo/pesospezenfermo.h5'  
+
+cnn =load_model(modelo_si_es_un_pez)
+cnn2 =load_model(modelopez_sano_o_enefermo)
+cnn3 =load_model(modelo_de_la_enfemedad)
+cnn.load_weights(pesos_modelo_si_es_un_pez) # si es pez 
+cnn2.load_weights(pesospez_sano_o_enefermo)# si es cual enfemedad
+cnn3.load_weights(pesos_modelo_de_la_enfemedad )#si esta enfermo
 def predict(file):
   x = load_img(file, target_size=(longitud, altura))
   x = img_to_array(x)
@@ -32,49 +51,58 @@ def predict(file):
   array = cnn.predict(x)
   result = array[0]
   answer = np.argmax(result)
-  if answer == 2:
-      print("prediccion: no es un pez")
+  if answer == 0:
+    print("prediccion:es un pez")
+    x2 = load_img(file, target_size=(longitud, altura))
+    x2 = img_to_array(x2)
+    x2 = np.expand_dims(x2, axis=0)
+    array = cnn2.predict(x2)
+    result = array[0]
+    answer = np.argmax(result)
+    if answer == 0:
+       print("prediccion: es un pez enefmo que tiene")
+       x3 = load_img(file, target_size=(longitud, altura))
+       x3 = img_to_array(x3)
+       x3 = np.expand_dims(x3, axis=0)
+       array = cnn3.predict(x3)
+       result = array[0]
+       answer = np.argmax(result)
+       if answer == 0:
+           print("prediccion: gusano lernea ")
+       elif answer ==1 :
+           print("prediccion: hidropecia ")
+       elif answer ==2 :
+           print("prediccion: huecos en la cabesa")
+       elif answer == 3:
+           print("prediccion: ich ")
+       elif answer ==4 :
+           print("prediccion: quemadura de bagre ")
+       elif answer == 5:
+           print("prediccion:  atcado o tumor y deformidad")
+       elif answer ==6 :
+           print("prediccion: branquias ")
+       elif answer == 7 :
+             print("prediccion: girodactilo ")
+       elif answer ==8 :
+           print("prediccion: hongos")
+       elif answer == 9:
+           print("prediccion: muerto ")
+       elif answer == 10:
+           print("prediccion: ojo picho ")
+       elif answer == 11:
+           print("parasito en la lengua")
+       elif answer == 12:
+           print("prediccion: podredumbre de aletas ")
+    elif answer == 1:
+        print("prediccion: es un pez sano")
+  elif answer == 1:
+    print("prediccion: no es un pez")
       #pare="noes.jpg"
       #noespez = cv2.imread(pare, cv2.IMREAD_COLOR)
       #noespezres = cv2.resize(noespez,None,fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
       #seleccion = cv2.add(noespezres,imagenpez)
       #cv2.imshow ('ventana2',seleccion)
-  elif answer == 0:
-    
-       print("prediccion: enfermo")
-       if answer == 0:
-             print("prediccion:  atcado o tumor y deformidad")
-       elif answer == 1:
-             print("prediccion: branquias ")
-       elif answer == 2:
-             print("prediccion: girodactilo ")
-       elif answer == 3:
-             print("prediccion: gusano lernea ")
-       elif answer == 4:
-             print("prediccion: hidropecia ")
-       elif answer == 5:
-             print("prediccion: hongos")
-       elif answer == 6:
-             print("prediccion: huecos en la cabesa")
-       elif answer == 7:
-             print("prediccion: ich ")
-       elif answer == 8:
-             print("prediccion: ojo picho ")
-       elif answer == 9:
-             print("prediccion: muerto ")
-       elif answer == 10:
-             print("parasito en la lengua")
-       elif answer == 11:
-             print("prediccion: podredumbre de aletas ")
-       elif answer == 12:
-             print("prediccion: quemadura de bagre ")
-       elif answer == 13:
-             print("prediccion: quemadura de bagre ")
 
-
-  elif answer== 1:
-      print("prediccion: sano")
-  
 
   return answer
 
--- entrenadorcurapeces.py
+++ entrenadorcurapeces.py
@@ -19,25 +19,34 @@ from tensorflow.python.keras import backend as K
 #varibles globales
 
 K.clear_session()
-
-especimendepractica = './especimendepractica/'
-especimendeexamen= './especimendeexamen/'
-pruebas = 2
+"""solo si es para  la enefemedad"""
+# especimendepractica = './especimendepractica/peces/enfermos/'
+# especimendeexamen= './especimendeexamen/peces/enfermos/'
+"""solo si es para  la sie eta sano"""
+especimendepractica = './especimendepractica/peces/'
+especimendeexamen= './especimendeexamen/peces/'
+"""solo si es para  si es un pez"""
+#especimendepractica = './especimendepractica'
+#especimendeexamen= './especimendeexamen'
+pruebas = 1
 alturadelaimagen =150
 longituddelaimagen= 150
-numerodeimagenesamandar=32
-pasos=700#numero de veces que se va aprosesar la informacion
-validacon=177
+numerodeimagenesamandar=64
+pasos=500#numero de veces que se va aprosesar la informacion
+validacion=183
 filtroprimeravez= 32
 filtrosegundavez= 64
-filtroterceravez= 96
+filtroterceravez= 32
 filtrocurtavez =128
+filtroquintavez =256
 filtrouno=(3,3)
 filtrodos=(2,2)
-filtrotres=(3,2)
-filtrocutro=(2,3)
+filtrotres=(3,3)
+filtrocutro=(4,4)
+filtroquinto=(5,5)
 pulido=(2,2)
-numerodenfermedades=3# cambiar mientras encuenbtro imagenes y la sano cuenta como enfermedad
+numerodenfermedades=2# cambiar mientras encuenbtro imagenes y la sano cuenta como enfermedad
+#numerodenfermedades=2
 lr = 0.00004
 
 
@@ -69,14 +78,17 @@ cnn.add(MaxPooling2D(pool_size=pulido))
 cnn.add(Convolution2D(filtrosegundavez, filtrodos, padding ="same"))
 cnn.add(MaxPooling2D(pool_size=pulido))
 
-cnn.add(Convolution2D(filtroterceravez, filtrotres, padding ="same"))
-cnn.add(MaxPooling2D(pool_size=pulido))
-
-cnn.add(Convolution2D(filtrocurtavez, filtrocutro, padding ="same"))
-cnn.add(MaxPooling2D(pool_size=pulido))
+# cnn.add(Convolution2D(filtroterceravez, filtrotres, padding ="same"))
+# cnn.add(MaxPooling2D(pool_size=pulido))
+#
+# cnn.add(Convolution2D(filtrocurtavez, filtrocutro, padding ="same"))
+# cnn.add(MaxPooling2D(pool_size=pulido))
+#
+# cnn.add(Convolution2D(filtroquintavez, filtroquinto, padding ="same"))
+# cnn.add(MaxPooling2D(pool_size=pulido))
 
 cnn.add(Flatten())
-cnn.add(Dense(256, activation='relu'))
+cnn.add(Dense(512, activation='relu'))
 cnn.add(Dropout(0.5))
 cnn.add(Dense(numerodenfermedades, activation='softmax'))
 
@@ -92,10 +104,15 @@ cnn.fit_generator(
     steps_per_epoch=pasos,
     epochs=pruebas,
     validation_data=validacion_generador,
-    validation_steps=validacon)
+    validation_steps=validacion)
 
 target_dir = './modelo/'
 if not os.path.exists(target_dir):
   os.mkdir(target_dir)
-cnn.save('./modelo/modelo.h5')
-cnn.save_weights('./modelo/pesos.h5')
+
+#cnn.save('./modelo/modelo.h5')
+# #cnn.save_weights('./modelo/pesos.h5')
+cnn.save('./modelo/modelopezenfermo.h5')
+cnn.save_weights('./modelo/pesospezenfermo.h5')
+# cnn.save('./modelo/modelopez.h5')
+# cnn.save_weights('./modelo/pesospez.h5')
